3a1e480be57e4fa617c365849ce7d84909df7a26
essi51 <essilhamdi51@gmail.com>
2025-07-05 12:07:48 +0000

    essil prime de la ville

--- my_print_ascii.c
+++ my_print_ascii.c
@@ -1,7 +1,16 @@
-void my_print_ascii(void);
-int  main(void)
-{
+void print_char(char c);
 
+void my_print_ascii(void)
+{
+    
+    for (int i = 33; i <128; ++i) {
+        print_char(i);
+    }
+
+}
+
+int main(void)
+{
     my_print_ascii();
     return 0;
 }
