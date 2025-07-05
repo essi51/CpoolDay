2e7e8ff4a3305981f3623d8c35e0ff37fc9f0082
essi51 <essilhamdi51@gmail.com>
2025-07-05 10:01:00 +0000

    my name=essil

---
+++ my_print_ascii.c
@@ -0,0 +1,7 @@
+void my_print_ascii(void);
+int  main(void)
+{
+
+    my_print_ascii();
+    return 0;
+}
---
+++ my_print_name.C
@@ -0,0 +1,8 @@
+void my_print_name(void){
+ my_print_char('e');
+ my_print_char('s');
+ my_priny_char('s');
+ my_priny_char('i');
+ my_print_char('l');
+
+ }
